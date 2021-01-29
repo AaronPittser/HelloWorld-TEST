18908c35d3b3da4bcf35dba51ae19dfc4b62397f
AaronPittser <47228138+AaronPittser@users.noreply.github.com>
2021-01-28 22:39:16 -0600

    main.cpp
    Main class for HelloWorld project.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ * Created on: Jan 28, 2021
+ *      Author: aaronpittser
+ */
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!\nMy name is Aaron Pittser.\nOne day, I'll be a White Hat hacker." << endl;
+  return 0;
+}
+  
